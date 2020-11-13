df47e25d7556f04b94db1f0b9d7d867ad2cc79cb
Brodie Gullic <brodiegullic@ou.edu>
2020-11-13 11:48:19 -0600

    Added date getter for instagram

--- activity-tracker/src/pages/ChapterPage.js
+++ activity-tracker/src/pages/ChapterPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {AppBar, Tabs, Tab, Card, CardContent, Grid} from '@material-ui/core';
 import { makeStyles} from '@material-ui/core/styles';
+import Profile from '../components/Profile'
 
 function TabPanel(props)
 {
@@ -18,66 +19,6 @@ function TabPanel(props)
   );
 }
 
-function Profile(props)
-{
-    const classes = useStyles();
-    const chapterInfo = props.chapterInfo;
-
-    return (
-        <>
-            <h2>Chapter Information</h2>
-            <Grid container spacing={0}>
-                <Grid item xs={3}>
-                    <h5 className={classes.grid}>Chapter Name </h5>
-                </Grid>
-                <Grid item xs={9}>
-                    <h5 className={classes.grid}>{chapterInfo.id}</h5>
-                </Grid>
-                <Grid item xs={3}>
-                    <h5 className={classes.grid}>Institution Name </h5>
-                </Grid>
-                <Grid item xs={9}>
-                    <h5 className={classes.grid}>{chapterInfo.institution}</h5>
-                </Grid>
-                <Grid item xs={3}>
-                    <h5 className={classes.grid}>Chapter Type </h5>
-                </Grid>
-                <Grid item xs={9}>
-                    <h5 className={classes.grid}>{chapterInfo.type}</h5>
-                </Grid>
-                <Grid item xs={3}>
-                    <h5 className={classes.grid}>Region </h5>
-                </Grid>
-                <Grid item xs={9}>
-                    <h5 className={classes.grid}>{chapterInfo.region}</h5>
-                </Grid>
-                <Grid item xs={3}>
-                    <h5 className={classes.grid}>Status</h5>
-                </Grid>
-                <Grid item xs={9}>
-                    <h5 className={classes.grid}>{chapterInfo.status}</h5>
-                </Grid>
-                <Grid item xs={3}>
-                    <h5 className={classes.grid}>Official Email Address </h5>
-                </Grid>
-                <Grid item xs={9}>
-                    <h5 className={classes.grid}>{chapterInfo.email}</h5>
-                </Grid>
-                <Grid item xs={3}>
-                    <h5 className={classes.grid}>Alternate Email Address </h5>
-                </Grid>
-                <Grid item xs={9}>
-                    <h5 className={classes.grid}>{chapterInfo.altemail}</h5>
-                </Grid>
-                <Grid item xs={3}>
-                    <h5 className={classes.grid}>Phone Number </h5>
-                </Grid>
-                <Grid item xs={9}>
-                    <h5 className={classes.grid}>{chapterInfo.phone}</h5>
-                </Grid>
-            </Grid>
-    </>);
-}
 
 const useStyles = makeStyles({
     card: {
@@ -106,7 +47,7 @@ function ChapterPage(props)
 
     const classes = useStyles();
     const [value, setValue] = useState(0);
-    const [url, setUrl] = useState();
+    const [instagram, setInstagram] = useState();
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
@@ -127,7 +68,7 @@ function ChapterPage(props)
         }
 
         getSite().then((response) => {
-            setUrl(response);
+            setInstagram(response);
         });
         
     }, [chapterInfo.handle]);
@@ -155,7 +96,10 @@ function ChapterPage(props)
             </Card>
         </TabPanel>
         <TabPanel value={value} index={2}>
-            {url !== undefined ? <iframe src={url.website + "embed/"} title="Instagram" width="20%" height="530"> </iframe> : null}
+            {instagram !== undefined ? 
+                <>
+                <iframe src={instagram.website + "embed/"} title="Instagram" width="20%" height="530"> </iframe> 
+                <h1>{instagram.day}</h1></> : null}
         </TabPanel>
         </>
     );
